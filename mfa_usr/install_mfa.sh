2f4446de77e01dea09baf7fefce771660c9a0322
Deok-Hyeon Cho <77186350+Choddeok@users.noreply.github.com>
2024-06-25 17:11:05 +0900

    Add files via upload

---
+++ mfa_usr/install_mfa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+pip uninstall -y typing
+pip install --ignore-requires-python git+https://github.com/MontrealCorpusTools/Montreal-Forced-Aligner.git@v2.0.0b3
+mfa thirdparty download
+apt install -y libopenblas-base libsox-fmt-mp3 libfst8 libfst-tools
+
+# sudo apt install -y libopenblas-base libsox-fmt-mp3 libfst8 libfst-tools
